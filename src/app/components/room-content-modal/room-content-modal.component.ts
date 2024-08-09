ab55196402c9fd6797b068d62b37af915d85d7ac
Hoang Phan <hoangphan29091999@gmail.com>
2024-08-09 23:09:05 +0700

    update nzParamsComponent to nzData

--- src/app/components/room-content-modal/room-content-modal.component.ts
+++ src/app/components/room-content-modal/room-content-modal.component.ts
@@ -1,34 +1,30 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { ItemData } from '../../interfaces/room';
-import { ProductService } from '../../services/product.service';
+import { Component, Inject } from '@angular/core';
+import { NzModalRef, NZ_MODAL_DATA } from 'ng-zorro-antd/modal';
 
 @Component({
   selector: 'app-room-content-modal',
   templateUrl: './room-content-modal.component.html',
   styleUrls: ['./room-content-modal.component.css']
 })
+export class RoomContentModalComponent {
+  roomData: any;
+  listData: any[] = [];
 
-export class RoomContentModalComponent implements OnInit {
-
-  @Input() roomData: any; // Assuming you will pass roomData as an Input
-
-  listData: any[] = []
-
-  constructor() { }
+  constructor(
+    private modalRef: NzModalRef<RoomContentModalComponent>,
+    @Inject(NZ_MODAL_DATA) private data: any // Nhận dữ liệu từ nzData
+  ) {
+    this.roomData = data.roomData;
+  }
 
   ngOnInit(): void {
-    this.fetchCurrentRoom()
+    this.fetchCurrentRoom();
   }
 
   fetchCurrentRoom(): void {
-    if (this.roomData.roomNumber) {
-      const lastEvents = this.roomData.events[this.roomData.events.length - 1]
-      this.listData = [lastEvents]
+    if (this.roomData && this.roomData.roomNumber) {
+      const lastEvents = this.roomData.events[this.roomData.events.length - 1];
+      this.listData = [lastEvents];
     }
   }
-
-
-
-
 }
-
