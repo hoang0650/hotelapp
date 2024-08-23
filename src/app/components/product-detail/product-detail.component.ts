afd98b57561e18eaf65f5bc5ae89e2d260dec0bb
Hoang Phan <hoangphan29091999@gmail.com>
2024-08-24 02:26:24 +0700

    update ng-zorro & visa, qr component

--- src/app/components/product-detail/product-detail.component.ts
+++ src/app/components/product-detail/product-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit , Input} from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { ProductService } from '../../services/product.service';
 import { ActivatedRoute } from '@angular/router';
 @Component({
